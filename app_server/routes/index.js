4f15f80fd72451ee2f711d4baa572f6ab0d55e77
datnhh <hoaidat.90@gmail.com>
2017-03-17 09:35:50 +0700

    add static file, and run node server first time

---
+++ app_server/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Video Conference' });
+});
+
+module.exports = router;
+
+// var express = require('express');
+// var router = express.Router();
+// var ctrlLocations = require('../controllers/locations');
+// var ctrlOthers = require('../controllers/others');
+
+// /* Locations pages */
+// router.get('/', ctrlOthers.angularApp);
+// router.get('/location/:locationid', ctrlLocations.locationInfo);
+// router.get('/location/:locationid/review/new', ctrlLocations.addReview);
+// router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
+
+// /* Other pages */
+// router.get('/about', ctrlOthers.about);
+
+// module.exports = router;

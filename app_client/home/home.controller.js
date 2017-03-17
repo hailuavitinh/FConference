b2e0310403263c52549cb87caad268b66653e7d2
datnhh <hoaidat.90@gmail.com>
2017-03-17 14:24:31 +0700

    homepage add

---
+++ app_client/home/home.controller.js
@@ -0,0 +1,48 @@
+(function () {
+
+  angular
+    .module('FConf')
+    .controller('homeCtrl', homeCtrl);
+
+  homeCtrl.$inject = ['$scope'];
+  function homeCtrl ($scope) {
+    
+    console.log('home controller');
+
+
+    // Nasty IE9 redirect hack (not recommended)
+    if (window.location.pathname !== '/') {
+      window.location.href = '/#' + window.location.pathname;
+    }
+    var vm = this;
+    console.log(window.location);
+    vm.pageHeader = {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    };
+    vm.sidebar = {
+      content: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
+    };
+    vm.message = "Checking your location";
+
+    vm.getData = function (position) {
+      var lat = position.coords.latitude,
+          lng = position.coords.longitude;
+      vm.message = "Searching for nearby places";      
+    };
+
+    vm.showError = function (error) {
+      $scope.$apply(function() {
+        vm.message = error.message;
+      });
+    };
+
+    vm.noGeo = function () {
+      $scope.$apply(function() {
+        vm.message = "Geolocation is not supported by this browser.";
+      });
+    };    
+
+  }
+
+})();
